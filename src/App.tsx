eaff68ef313de0733c1bb6abf8ea0ef63fcedf7a
scriblesean <sean.arackal@gmail.com>
2024-04-18 16:49:21 -0400

    objected oriented done

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
 import './App.css'
-import {TaxStatusOO, Context} from "./TaxStatusOO.ts";
+import {TaxStatusOO, Context, Single, Married, Separated} from "./TaxStatusOO.ts";
 
 function App() {
     const income: number = Number(prompt("Enter your income", "0"));
 
     // Object-oriented code. Complete the code below.
     // Create the classes
-    const single: TaxStatusOO;
-    const married: TaxStatusOO;
-    const separated: TaxStatusOO;
-    const taxStatusSelection: Context;
+    const single: TaxStatusOO = new Single();
+    const married: TaxStatusOO = new Married();
+    const separated: TaxStatusOO = new Separated();
+    const taxStatusSelection: Context = new Context(single);
 
     // calculate the tax by using the Context class
-    const singleTaxOO: number;
+    const singleTaxOO: number = taxStatusSelection.calculateTax(income);
 
     // Use the setter to change the TaxStatus class to Married and calculate the tax
-    const marriedTaxOO: number;
+    taxStatusSelection.taxStatus = married;
+    const marriedTaxOO: number = taxStatusSelection.calculateTax(income);
 
     // Use the setter to change the TaxStatus class to Separated and calculate the tax
-    const separatedTaxOO: number;
+    taxStatusSelection.taxStatus = separated;
+    const separatedTaxOO: number = taxStatusSelection.calculateTax(income);
 
     // Functional programming code.
     // Calculate the tax using the higher-order function selectTaxStrategy()
-    const singleTaxFP: number;
-    const marriedTaxFP: number;
-    const separatedTaxFP: number;
+    // The functional programming part is not completed as the functions are not defined yet.
 
     // Fill in the variables for the code below
     return (
         <>
             <h2>Tax Strategy Pattern - Object Oriented</h2>
-            <b>For an income of ${}, the tax is</b>
-            <br/><b>${}</b> for a <b>single</b> person
-            <br/><b>${}</b> for a <b>married</b> couple
-            <br/><b>${}</b> for a <b>married couple filing separately</b>
+            <b>For an income of ${income}, the tax is</b>
+            <br/><b>${singleTaxOO}</b> for a <b>single</b> person
+            <br/><b>${marriedTaxOO}</b> for a <b>married</b> couple
+            <br/><b>${separatedTaxOO}</b> for a <b>married couple filing separately</b>
 
             <h2>Tax Strategy Pattern - Functional Programming</h2>
-            <b>For an income of ${}, the tax is</b>
-            <br/><b>${}</b> for a <b>single</b> person
-            <br/><b>${}</b> for a <b>married</b> couple
-            <br/><b>${}</b> for a <b>married couple filing separately</b>
+            <b>For an income of ${income}, the tax is</b>
+            <br/><b>${/*singleTaxFP*/}</b> for a <b>single</b> person
+            <br/><b>${/*marriedTaxFP*/}</b> for a <b>married</b> couple
+            <br/><b>${/*separatedTaxFP*/}</b> for a <b>married couple filing separately</b>
         </>
     );
 }
--- src/TaxStatusOO.ts
+++ src/TaxStatusOO.ts
@@ -4,14 +4,72 @@ export interface TaxStatusOO {
 
 export class Single {
     public calculateTax(income: number): number {
-
+        if(income <= 19999) {
+            return income * 0.12;
+        }
+        else if(income <= 49999) {
+            return income * 0.15;
+        }
+        else {
+            return income * 0.20;
+        }
     }
 }
 
 // create the class Married
 
+export class Married {
+    public calculateTax(income: number): number {
+        if(income <= 49999) {
+            return income * 0.12;
+        }
+        else if(income <= 99999) {
+            return income * 0.15;
+        }
+        else {
+            return income * 0.20;
+        }
+    }
+
+}
+
 // create the class Separated
+
+export class Separated {
+    public calculateTax(income: number): number {
+        if(income <= 15999) {
+            return income * 0.12;
+        }
+        else if(income <= 40000) {
+            return income * 0.15;
+        }
+        else if(income <= 74999) {
+            return income * 0.20;
+
+        }
+        else {
+            return income * 0.25;
+        }
+    }
+
+}
 
 export class Context {
     private _taxStatus: TaxStatusOO;
+
+    constructor(taxStatus: TaxStatusOO) {
+        this._taxStatus = taxStatus;
+    }
+
+    get taxStatus(): TaxStatusOO {
+        return this._taxStatus;
+    }
+
+    set taxStatus(value: TaxStatusOO) {
+        this._taxStatus = value;
+    }
+
+    calculateTax(income: number): number {
+        return this._taxStatus.calculateTax(income);
+    }
 }
